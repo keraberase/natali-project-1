8eac5247a77e80190b2b6e5611a30ffa73704934
keraberas <46618649+keraberase@users.noreply.github.com>
2024-11-01 18:45:48 +0000

    test vercel

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Определение папки для статических файлов
+app.use('/assets', express.static(path.join(__dirname, 'dist/assets')));
+
+// Маршрут для index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
