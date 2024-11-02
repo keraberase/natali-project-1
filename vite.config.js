8a25d11afa6c6eedfbeb5fd493d65bd485763581
keraberas <46618649+keraberase@users.noreply.github.com>
2024-11-02 18:34:16 +0000

    test certificat

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,41 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import {glob} from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
-import viteStaticCopy from 'vite-plugin-static-copy';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-export default defineConfig(({ command }) => ({
-  define: {
-    [command === 'serve' ? 'global' : '_global']: {},
-  },
-  root: 'src',
-  base: command === 'build' ? './' : '/',
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      input: glob.sync('./src/**/*.html'),
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-        entryFileNames: chunkInfo => (chunkInfo.name === 'commonHelpers' ? 'commonHelpers.js' : '[name].js'),
-        assetFileNames: assetInfo =>
-          assetInfo.name && assetInfo.name.endsWith('.html') ? '[name].[ext]' : 'assets/[name]-[hash][extname]',
-      },
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
     },
-    outDir: './dist',
-    emptyOutDir: true,
-  },
-  plugins: [
-    injectHTML(),
-    FullReload(['./src/**/*.html']),
-    SortCss({
-      sort: 'mobile-first',
-    }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/images/*',
-          dest: 'assets',
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
         },
-      ],
-    }),
-  ],
-}));
+      },
+      outDir: '../dist',
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/*.html']),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'images/*', // Путь к вашим изображениям
+            dest: 'images'    // Куда копировать
+          }
+        ],
+      }),
+    ],
+  };
+});
