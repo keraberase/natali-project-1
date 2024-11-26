7474b3ae460e4a9d05a6a6186511b8bfd5ff9a8e
keraberas <46618649+keraberase@users.noreply.github.com>
2024-11-26 16:48:27 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -4,45 +4,41 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+export default defineConfig({
+  base: '/natali-project-1/',  // Указываем базовый путь подкаталога
+  root: 'src',
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        entryFileNames: chunkInfo => {
+          if (chunkInfo.name === 'commonHelpers') {
+            return 'commonHelpers.js';
+          }
+          return '[name].js';
+        },
+        assetFileNames: assetInfo => {
+          if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+            return '[name].[ext]';
+          }
+          return 'assets/[name]-[hash][extname]';
         },
       },
-      outDir: '../dist',
-      emptyOutDir: true,
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  };
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/**.html']),
+    SortCss({
+      sort: 'mobile-first',
+    }),
+  ],
 });
